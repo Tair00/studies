dadc819d485f575eff8dc9e9ca44f67c18e70e4c
Tair00 <suleimanov.t.i.20@gmail.com>
2022-06-26 12:24:10 +0300

    Mediator

---
+++ src/IChatRoom.java
@@ -0,0 +1,7 @@
+
+public interface IChatRoom
+{
+    public void sendMessage(String msg, String userId);
+
+    void addUser(User user);
+}
---
+++ src/main.java
@@ -0,0 +1,20 @@
+public class main
+{
+    public static void main(String[] args)
+    {
+        IChatRoom chatroom = new ChatRoom();
+
+        User user1 = new ChatUser(chatroom,"1", "Alex");
+        User user2 = new ChatUser(chatroom,"2", "Brian");
+        User user3 = new ChatUser(chatroom,"3", "Charles");
+        User user4 = new ChatUser(chatroom,"4", "David");
+
+        chatroom.addUser(user1);
+        chatroom.addUser(user2);
+        chatroom.addUser(user3);
+        chatroom.addUser(user4);
+
+        user1.send("Java это гуд", "2");
+        user2.send("Tair ты надоел ", "1");
+    }
+}
